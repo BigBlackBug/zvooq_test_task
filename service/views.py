abf502b7d24a820af6804da2342ee37294a3c396
bigblackbug <bigblackbugg@gmail.com>
2017-07-01 01:48:21 +0300

    changed all log output to 'info'

--- service/views.py
+++ service/views.py
@@ -17,20 +17,20 @@ class MainView(View):
 
     def get(self, request):
         key = request.GET.get("key")
-        logger.debug("Got request for key {}".format(key))
+        logger.info("Got request for key {}".format(key))
         # TODO input validation
         redis_client = redis.Redis(connection_pool=self.pool)
-        logger.debug("Checking if key {} is in cache".format(key))
+        logger.info("Checking if key {} is in cache".format(key))
         hash = redis_client.get(key)
         if hash:
-            logger.debug("Key {} is in cache. Value: {}".format(key, hash))
+            logger.info("Key {} is in cache. Value: {}".format(key, hash))
             return JsonResponse({
                 "hash": hash
             })
         else:
-            logger.debug("Key {} not found in cache")
+            logger.info("Key {} not found in cache")
             if not redis_client.sismember("in_progress", key):
-                logger.debug("Task for key {} is not running. "
+                logger.info("Task for key {} is not running. "
                              "Starting".format(key))
                 redis_client.sadd("in_progress", key)
                 service_request_task.delay(key)
