5edeaac740d491a7264a69a0b2fece0ec0f85c2b
bigblackbug <bigblackbugg@gmail.com>
2017-07-01 03:07:01 +0300

    added retry logic

--- service/tasks.py
+++ service/tasks.py
@@ -3,28 +3,42 @@ import logging
 import redis
 import requests
 from celery import shared_task
+from celery.exceptions import MaxRetriesExceededError
+from requests import RequestException
 
 _SERVICE_URL = "https://vast-eyrie-4711.herokuapp.com/?key={}"
 
 pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
 
 SECONDS_IN_DAY = 60 * 60 * 24
-
+connect_timeout, read_timeout = 5.0, 10.0
 logger = logging.getLogger(__name__)
 
 
-@shared_task
-def service_request_task(key):
+@shared_task(bind=True, default_retry_delay=3, max_retries=5)
+def service_request_task(self, key):
+    def handle_retry(task):
+        try:
+            task.retry()
+        except MaxRetriesExceededError:
+            redis_client.srem("in_progress", key)
+            logger.exception(e)
+
     logger.info("Started task for key {}".format(key))
     redis_client = redis.Redis(connection_pool=pool)
-    # TODO if request takes too long, retry
-    response = requests.get(_SERVICE_URL.format(key))
-    logger.info(
-        "Got response from the remote server: '{}'".format(response.content))
-    if response.content == 'error':
-        # TODO retry
-        logger.error("Remote server returned an error")
+    try:
+        response = requests.get(_SERVICE_URL.format(key),
+                                timeout=(connect_timeout, read_timeout))
+    except RequestException as e:
+        handle_retry(self)
     else:
-        response_data = response.json()
-        redis_client.set(key, response_data['hash'], ex=SECONDS_IN_DAY)
-        redis_client.srem("in_progress", key)
+        logger.info(
+            "Got response from the remote server: '{}'".format(
+                response.content))
+        if response.content == 'error':
+            logger.error("Remote server returned an error")
+            handle_retry(self)
+        else:
+            response_data = response.json()
+            redis_client.set(key, response_data['hash'], ex=SECONDS_IN_DAY)
+            redis_client.srem("in_progress", key)
